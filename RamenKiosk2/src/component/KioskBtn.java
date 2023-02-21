dbf31e04827d2809deffb528bfdcf3d5c4cae790
hyeg0121 <hyeg0121@naver.com>
2023-02-21 21:32:10 +0900

    수정

--- RamenKiosk2/src/component/KioskBtn.java
+++ RamenKiosk2/src/component/KioskBtn.java
@@ -9,9 +9,9 @@ import javax.swing.JButton;
 public class KioskBtn extends JButton {
 	
 	public KioskBtn(String imgName) {
-		setIcon(new ImageIcon(KioskBtn.class.getResource("../images/startBtn.png")));
-		setRolloverIcon(new ImageIcon(KioskBtn.class.getResource("../images/startBtn.png")));
-		setRolloverSelectedIcon(new ImageIcon(KioskBtn.class.getResource("../images/startBtn.png")));
+		setIcon(new ImageIcon(KioskBtn.class.getResource("../images/"+imgName+".png")));
+		setRolloverIcon(new ImageIcon(KioskBtn.class.getResource("../images/"+imgName+".png")));
+		setRolloverSelectedIcon(new ImageIcon(KioskBtn.class.getResource("../images/"+imgName+".png")));
 		setFocusPainted(false);
 		setBackground(Color.WHITE);
 		setBorder(null);
