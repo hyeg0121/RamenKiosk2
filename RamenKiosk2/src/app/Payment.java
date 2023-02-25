fc2b0365db84cd4062a4ead54f1527bc98458e70
hyeg0121 <hyeg0121@naver.com>
2023-02-25 11:50:10 +0900

    결제 페이지 생성 (컴포넌트 배치 X)

---
+++ RamenKiosk2/src/app/Payment.java
@@ -0,0 +1,14 @@
+package app;
+
+import component.KioskFrame;
+
+public class Payment extends KioskFrame {
+	static int ramenPrice = 0;
+	static int otherPrice = 0;
+	
+	
+	public static void main(String[] args) {
+		new Payment().setVisible(true);
+	}
+
+}
