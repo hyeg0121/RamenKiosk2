5fe24034752522c159725427cfa03c938c7045c0
hyeg0121 <hyeg0121@naver.com>
2023-02-28 08:38:05 +0900

    버튼에 해당하는 창으로 이동함

--- RamenKiosk2/src/app/EarnPoint.java
+++ RamenKiosk2/src/app/EarnPoint.java
@@ -8,20 +8,29 @@ public class EarnPoint extends KioskFrame {
 	
 	public EarnPoint() {
 		KioskPanel pnl = new KioskPanel(getImage("pointBg"));
-		add(setBounds(pnl, -5, -5, this.getWidth(), this.getHeight()));
+		add(pnl);
 		
 		KioskBtn btn[] = new KioskBtn[3];
 		for (int i = 0; i < btn.length; i++) {
 			btn[i] = new KioskBtn("point"+(i+1));
 			pnl.add(setBounds(btn[i], 125, 360 + i*110 , 200, 60));
 			
-			if(i == 2) {
-				btn[i].addActionListener(e ->{
-					new SingUp().setVisible(true);
-					dispose();
-				});
-			}
 		}
+		
+		btn[0].addActionListener(e -> {
+			new LogIn().setVisible(true);
+			dispose();
+		});
+		
+		btn[1].addActionListener(e -> {
+			new Success().setVisible(true);
+			dispose();
+		});
+		
+		btn[2].addActionListener(e -> {
+			new SignUp().setVisible(true);
+			dispose();
+		});
 	}
 	public static void main(String[] args) {
 		new EarnPoint().setVisible(true);
