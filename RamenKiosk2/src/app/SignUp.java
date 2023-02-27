034dc9ada209bc03bc56fd2d7c374e480f99dcae
hyeg0121 <hyeg0121@naver.com>
2023-02-28 08:47:15 +0900

    회원가입 창 컴포넌트 배치

--- RamenKiosk2/src/app/SignUp.java
+++ RamenKiosk2/src/app/SignUp.java
@@ -1,21 +1,42 @@
 package app;
 
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 import component.KioskBtn;
 import component.KioskFrame;
 import component.KioskPanel;
 
-public class SingUp extends KioskFrame {
+public class SignUp extends KioskFrame {
 
-	public SingUp() {
+	public SignUp() {
 		KioskPanel pnl = new KioskPanel(getImage("signUpBg"));
-		add(setBounds(pnl, -5, -5, this.getWidth(), this.getHeight()));
+		add(pnl);
 		
 		KioskBtn btn = new KioskBtn("signUpBtn");
 		pnl.add(setBounds(btn, 125, 540, 200, 60));
+		
+		JLabel nameLb = new JLabel("이름");
+		nameLb.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+		pnl.add(setBounds(nameLb, 60, 240, 100, 35));
+		
+		JLabel pwLb = new JLabel("비밀번호");
+		pwLb.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+		pnl.add(setBounds(pwLb, 60, 360, 100, 35));
+		
+		JTextField nameTf = new JTextField();
+		pnl.add(setBounds(nameTf, 60, 290, 300, 35));
+		
+		JPasswordField pwTf = new JPasswordField();
+		pnl.add(setBounds(pwTf, 60, 410, 300, 35));
+		
 	}
 
 	public static void main(String[] args) {
-		new SingUp().setVisible(true);
+		new SignUp().setVisible(true);
 	}
 
 }
