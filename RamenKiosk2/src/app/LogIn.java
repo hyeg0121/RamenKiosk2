28e64b3bbe1dc826b354145cffad5d5a9fcdbdb8
hyeg0121 <hyeg0121@naver.com>
2023-03-01 20:57:40 +0900

    컴포넌트 배치

--- RamenKiosk2/src/app/LogIn.java
+++ RamenKiosk2/src/app/LogIn.java
@@ -1,5 +1,14 @@
 package app;
 
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import component.KioskBtn;
 import component.KioskFrame;
 import component.KioskPanel;
 
@@ -7,6 +16,26 @@ public class LogIn extends KioskFrame{
 
 	public LogIn() {
 		KioskPanel pnl = new KioskPanel(getImage("logInBg"));
+		add(pnl);
+		
+		KioskBtn btn = new KioskBtn("logInBtn");
+		pnl.add(setBounds(btn, 125, 540, 200, 60));
+		
+		JLabel nameLb = new JLabel("이름");
+		nameLb.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+		pnl.add(setBounds(nameLb, 60, 240, 100, 35));
+		
+		JLabel pwLb = new JLabel("비밀번호");
+		pwLb.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+		pnl.add(setBounds(pwLb, 60, 360, 100, 35));
+		
+		JTextField nameTf = new JTextField();
+		pnl.add(setBounds(nameTf, 60, 290, 300, 35));
+		
+		JPasswordField pwTf = new JPasswordField();
+		pnl.add(setBounds(pwTf, 60, 410, 300, 35));
+		
+		
 	}
 
 	public static void main(String[] args) {
