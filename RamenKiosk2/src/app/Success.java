7e3f6c868f2db04a839560a0ad718b8c4ea06be7
hyeg0121 <hyeg0121@naver.com>
2023-03-05 08:20:59 +0900

    조리중 페이지로 이동하는 버튼 생성

--- RamenKiosk2/src/app/Success.java
+++ RamenKiosk2/src/app/Success.java
@@ -1,5 +1,6 @@
 package app;
 
+import component.KioskBtn;
 import component.KioskFrame;
 import component.KioskPanel;
 
@@ -7,6 +8,14 @@ public class Success extends KioskFrame{
 
 	public Success() {
 		KioskPanel pnl = new KioskPanel(getImage("successBg"));
+		add(pnl);
+		
+		KioskBtn btn = new KioskBtn("successBtn");
+		pnl.add(setBounds(btn, 125, 500, 200, 60));
+		btn.addActionListener(e ->{
+			new Cooking().setVisible(true);
+			dispose();
+		});
 	}
 
 	public static void main(String[] args) {
